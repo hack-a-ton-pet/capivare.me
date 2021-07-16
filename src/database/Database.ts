34b03a63412c04e2ac8a3f5f6fb4e30e152eb493
Joao Pedro Silveira <jpedross1999@gmail.com>
2021-07-16 20:08:15 -0300

    Fix:  user not exists (yet)

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,21 +1,14 @@
 import Dexie from 'dexie'
 import Table from '../type/Table'
-import User from '../type/User'
 
 const NAME = 'CAPICRACIA'
 const VERSION = 1
 
 class Database extends Dexie {
-	user: Table<User>
-
 	constructor() {
 		super(NAME)
 
-		this.version(VERSION).stores({
-			user: generateIndexes('cpf'),
-		})
-
-		this.user = this.table('user')
+		this.version(VERSION).stores({})
 	}
 }
 
