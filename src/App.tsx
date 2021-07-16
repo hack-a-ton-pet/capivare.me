23bff77b0e28bc5e0c5fce7d7dd1785479dcb7ad
Mayra <mayra.cademartori@gmail.com>
2021-07-16 19:22:14 -0300

    Feat: Routes e Cards MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import './App.css'
+import React from "react"
+import "./App.css"
+import { Switch, Route, Router, Link } from "react-router-dom"
+import PathConstants from "./constant/PathConstants"
+import Menu from "./view/menu"
+import Login from "./view/login"
+import { createBrowserHistory } from "history"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
+function App() {
+	return (
+		<div className="App">
+			<Router history={createBrowserHistory()}>
+				<nav>
+					<ul>
+						<li>
+							<Link to={PathConstants.LOGIN}>LOGIN</Link>
+						</li>
+						<li>
+							<Link to={PathConstants.MENU}>MENU</Link>
+						</li>
+						<li>
+							<Link to={PathConstants.LEARN}>LEARN</Link>
+						</li>
+						<li>
+							<Link to={PathConstants.QUIZ}>QUIZ</Link>
+						</li>
+					</ul>
+				</nav>
+				<Switch>
+					<Route exact path={PathConstants.LOGIN}>
+						<Login />
+					</Route>
+					<Route exact path={PathConstants.MENU}>
+						<Menu />
+					</Route>
+					<Route path={PathConstants.LEARN} component={() => <></>} />
+					<Route path={PathConstants.QUIZ} component={() => <></>} />
+					<Route path={"/"} component={() => <></>} />
+				</Switch>
+			</Router>
+		</div>
+	)
 }
 
 export default App
---
+++ src/constant/PathConstants.ts
@@ -0,0 +1,9 @@
+class PathConstants {
+	ROOT = "localhost:3000"
+	LOGIN = `/`
+	MENU = `/menu`
+	LEARN = `/learn`
+	QUIZ = `/quiz`
+}
+
+export default new PathConstants()
