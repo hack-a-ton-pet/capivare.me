3cd05db4e171831eb004f89afec3e1cc4d2d3d2b
Mayra <mayra.cademartori@gmail.com>
2021-07-17 17:32:27 -0300

    Feat(final dialog lesson going to quizzes)

--- src/view/lesson/lesson_section/index.tsx
+++ src/view/lesson/lesson_section/index.tsx
@@ -6,25 +6,46 @@ import ArrowNextIconButton from '../../../component/icon_button/arrow_next'
 import CapiStepper from '../../../component/stepper'
 import GoBackIconButton from '../../../component/icon_button/go_back'
 import './styles.css'
+import LearnPathCard from '../../../component/learn_path_card'
+import HistoryService from '../../../service/history/HistoryService'
+import PathConstants from '../../../constant/PathConstants'
 
 const LessonSection: React.FC = () => {
 	const [index, setIndex] = useState(0)
 
 	const handleChangeIndex = (index: number) => {
-		if (index > -1 && index < learnCards.length) setIndex(index)
+		if (index > -1 && index < learnCards.length + 1) setIndex(index)
 	}
 
 	const incrementIndex = (inc: number) => {
 		handleChangeIndex(index + inc)
 	}
 
+	const handleClick = () => {
+		setTimeout(() => HistoryService.push(PathConstants.QUIZ), 200)
+	}
+
+	const renderDialogs = () => {
+		const array = learnCards.map((e, index) => (
+			<ContentCard key={index} title={e.title} paragraphs={e.text} />
+		))
+		array.push(
+			<LearnPathCard
+				buttonText='Pratique'
+				title='Você terminou!'
+				description='Agora vamos praticar o que aprendemos!'
+				id='section1'
+				onClick={handleClick}
+			/>,
+		)
+		return array
+	}
+
 	return (
 		<div className='lesson_section'>
 			<GoBackIconButton color='primary' />
 			<SwipeableViews index={index} onChangeIndex={handleChangeIndex}>
-				{learnCards.map((e, index) => (
-					<ContentCard key={index} title={e.title} paragraphs={e.text} />
-				))}
+				{renderDialogs()}
 			</SwipeableViews>
 			<div className='stepper_menu'>
 				<ArrowNextIconButton
@@ -32,7 +53,7 @@ const LessonSection: React.FC = () => {
 					direction='back'
 					onClick={() => incrementIndex(-1)}
 				/>
-				<CapiStepper steps={learnCards.length} current={index} />
+				<CapiStepper steps={learnCards.length + 1} current={index} />
 				<ArrowNextIconButton
 					color='primary'
 					onClick={() => incrementIndex(1)}
