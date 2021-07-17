3534c3f2625147f4b5f2c81d90af21dce121a9df
Bernardo Beneduzi <bernardobeneduzi@gmail.com>
2021-07-17 12:15:50 -0300

    Feat: section card done

--- src/component/section_card/index.tsx
+++ src/component/section_card/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import CapiButton from '../button'
 import './styles.css'
 
-const SectionCard: React.FC = () => {
+export interface SectionCardProps {
+    title: string
+    description: string
+    buttonText: string
+    id: string
+    onClick: () => void
+}
+
+const SectionCard: React.FC<SectionCardProps> = ({ title, description, buttonText, id, onClick }) => {
     return (
-        <div className="section_card">
-            <h1>oioioi</h1>
+        <div id={id} className="section_card">
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <div className="button_wrapper">
+                <CapiButton text={buttonText} onClick={onClick} />
+            </div>
         </div>
     )
 }
