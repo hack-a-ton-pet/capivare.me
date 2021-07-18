a1d5ac20347aff7c43ccc4ae87c245cf8129bde6
Bernardo Beneduzi <bernardobeneduzi@gmail.com>
2021-07-18 10:57:45 -0300

    Feat: logout button

--- src/view/menu/index.tsx
+++ src/view/menu/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { authStore } from '../../context/AuthContext'
+import LogoutIconButton from '../../component/icon_button/logout'
 import menuCards from '../../constant/data/menuCard'
 import CapiCard from '../../component/card'
 import './styles.css'
 
 const Menu: React.FC = () => {
+	const { dispatch } = useContext(authStore)
+	const handleClick = () => {
+	}
+
 	return (
 		<div className='menu'>
 			{menuCards.map((e, index) => {
 				return <CapiCard key={index} item={e} />
 			})}
+			<div className="logout-wrapper">
+				<LogoutIconButton onClick={() => handleClick() />
+			</div>
 		</div>
 	)
 }
