8536dfb16aa832c15ba10b5ec4c742708ed5a264
Joao Pedro Silveira <jpedross1999@gmail.com>
2021-07-18 13:30:24 -0300

    Fix: merge error

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -32,8 +32,8 @@ const Main: React.FC = () => {
 					path={`${Path.LESSON}/:id`}
 					component={LessonSection}
 				/>
-				<AuthenticatedRoute path={`${Path.QUIZ}`} component={Quiz} />
 				<AuthenticatedRoute path={`${Path.QUIZ}/:id`} component={Quiz} />
+				<AuthenticatedRoute exact path={`${Path.QUIZ}`} component={Quiz} />
 				<AuthenticatedRoute
 					exact
 					path={Path.ACHIEVEMENTS}
